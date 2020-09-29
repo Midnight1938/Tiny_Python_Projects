c491436a5899869dfcdbaa92b8cb3822544ccc90
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-09-29 23:33:43 +0530

    Added paraoption

--- Word_counter.py
+++ Word_counter.py
@@ -1,6 +1,14 @@
-Text = str(input("Input text to count: \n"))
+Lines = int(input("Enter the number of paragraphs you have: (numeric)"))
+lines = ''
+i = 0
+while i in range(Lines):
+    lines += input()+"\n"
+    Text = str(input("Input text in your para number {}: \n".format(i+1)))
+    i += 1
 
-Split_lines = Text.split("\n")
-Split_words = Text.split(" ")
+Split_lines = lines.split("\n")
+Split_words = lines.split(" ")
 
-print("The text is {} words and spans {} lines".format(len(Split_words), len(Split_lines)))
+
+print("The text is {} words and spans {} lines".format(
+    len(Split_words), len(Split_lines)))
