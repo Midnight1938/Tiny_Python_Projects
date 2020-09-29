aaa0d1f3112c7947859ee793ccdcbfeb01f7ce2e
Midnight1938 <dhoomsaksham2004@gmail.com>
2020-09-29 23:39:06 +0530

    Fixed bugs

--- Word_counter.py
+++ Word_counter.py
@@ -1,14 +1,15 @@
-Lines = int(input("Enter the number of paragraphs you have: (numeric)"))
+Lines = int(input("Enter the number of paragraphs you have (numeric): "))
 lines = ''
 i = 0
+
 while i in range(Lines):
-    lines += input()+"\n"
-    Text = str(input("Input text in your para number {}: \n".format(i+1)))
+    Text = str(input("Input text in your para number {}: ".format(i+1)))
+    lines += Text+"\n"
     i += 1
 
 Split_lines = lines.split("\n")
 Split_words = lines.split(" ")
 
-
+print(lines)
 print("The text is {} words and spans {} lines".format(
-    len(Split_words), len(Split_lines)))
+    len(Split_words)+1, len(Split_lines)-1))
